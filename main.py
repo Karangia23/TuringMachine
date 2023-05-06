fea9aaaf8656ff1f796fec58d2522592c2b6a7f2
Karangia23 <karangia3984@op.pl>
2023-05-06 17:59:31 +0200

    Added basic functionality

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
+import re
 
-class turingMachine:
-    def __init__(self):
-        pass
+class TurMachine:
+    def __init__(self,file,tape=[]):
+        self.alphabet
+        self.tape = tape
+        self.head = 0
+        self.states
+        self.currentState
+
+        with open(file, "r") as f:
+            for line in f:
+                if line == 1:
+                    pass
+                elif line == 2:
+                    pass
+                else:
+
+
+
+    def headRead(self):
+        return self.tape[self.head]
+    def headWrite(self, value):
+        self.tape[self.head] = value 
+    def moveLeft(self):
+        if(self.head <= 0):
+            raise Exception("Head index cannot be lower than 0")
+        else:
+            self.head -= 1
+    def moveRight(self):
+        if(self.head >= len(self.tape)-1):
+            raise Exception("Head index cannot be larger than tape length")
+        else:
+            self.head += 1
+    def changeState(self, stateName):
+        self.currentState = self.states[stateName]
+    def translateState(self):
