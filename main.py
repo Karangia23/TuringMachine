d2098d928bd308160630b1b5f18d4536c3861f68
Karangia23 <karangia3984@op.pl>
2023-06-02 17:35:48 +0200

    Working implementation of turingMachine

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from functools import partial
 
 class TurMachine:
     def __init__(self,file):
-        self.turMachineName
+        self.turMachineName = ""
         self.alphabet = []
         self.tape = []#[[1,2,3,4],[-,-,-,-,-,-]]
         self.states = {} #{state1:{AZ: [a(),b(),c()]; BZ: doZ}}
-        self.currentState
-        self.acceptState
+        self.currentState = ""
+        self.acceptState = ""
         self.head = []
         self.tapeNumber = 0
         self.nameStates = []
@@ -19,48 +19,69 @@ class TurMachine:
                 if lineNumber == 0:
                     self.turMachineName = line
                 elif lineNumber == 1:
-                    self.acceptState = line
+                    self.currentState = line.rstrip()
                 elif lineNumber == 2:
                     self.tapeNumber = (int(line))
                     for i in range(int(line)):
                         self.tape.append([])
                         self.head.append(0)
+                elif lineNumber == 3:
+                    self.acceptState = line.rstrip()
                 elif line[0].isalpha():
-                    reStateName = re.search("^(\w)[\w]+,", line)
+                    line = line.replace(" ", "")
+                    reStateName = re.search("^(\w)[\w]+(?=,)", line)
                     stateName = reStateName.group()
+                    stateName.replace(" ", "")
                     reWhatToLookUp = re.search("(?<=,).+", line)
                     valuesInTapes = reWhatToLookUp.group()
+                    valuesInTapes.replace(" ", "")
                     temp = valuesInTapes.split(',')
                     valuesInTapes = "".join(str(x) for x in temp)
-                    next(f)
-                    reNewState = re.search("^(\w)[\w]+,", line)
+                    valuesInTapes.replace(" ", "")
+                    line = next(f)
+                    lineNumber += 1
+                    reNewState = re.search("^(\w)[\w]+(?=,)", line)
                     newState = reNewState.group()
+                    newState.replace(" ", "")
                     temp = re.search("(?<=,).+", line)
-                    whatToDo = "".join(str(x) for x in temp)
+                    whatToDo = temp.group().replace(" ", "").split(',')
                     j = []
                     for i in range(len(self.tape)):
                         k = partial(self.headWrite, whatToDo[i], i)
                         j.append(k)
-                    for i in range(len(self.tape), len(whatToDo)-1):
-                        currentTape = i-len(self.tape)
+                    for i in range(len(whatToDo)):
+
+                        currentTape = i
+                        if currentTape > len(self.tape)-1:
+                            currentTape = i - len(self.tape)
+                        
                         if whatToDo[i] == '<':
                             k= partial(self.moveLeft, currentTape)
                             j.append(k)
                         elif whatToDo[i] == '>':
                             k = partial(self.moveRight, currentTape)
                             j.append(k)
+                        elif whatToDo[i] != '-':
+                            k = partial(self.headWrite, whatToDo[i], currentTape)
                     k = partial(self.changeState, newState)
                     j.append(k)
-                    self.states[stateName][valuesInTapes][j]
-                    
+                    try:
+                        self.states[stateName][valuesInTapes] = j
+                    except:
+                        self.states.update({stateName:{valuesInTapes:j}})
+
                         
                     #sczytac dwie linijki
                     #tu co na pierwszej linijce
                 lineNumber += 1
 
     def headRead(self, tapeNumber = 0):
-        tape = self.tape[tapeNumber]
-        return tape[self.head[tapeNumber]]
+        try:
+            tape = self.tape[tapeNumber]
+            return tape[self.head[tapeNumber]]
+        except:
+            self.tape[tapeNumber].append('_')
+            return '_'
     def headWrite(self, value, tapeNumber = 0):
         try:
             self.tape[tapeNumber][self.head[tapeNumber]] = value
@@ -69,23 +90,38 @@ class TurMachine:
     def moveLeft(self, tapeNumber=0):
         self.head[tapeNumber] -= 1
         if self.head[tapeNumber] < 0:
-            self.tape[tapeNumber]
+            self.tape[tapeNumber].insert(0, "_")
+            self.head[tapeNumber] = 0 
     def moveRight(self, tapeNumber = 0):
-        self.head[tapeNumber] -= 1
+        self.head[tapeNumber] += 1
     def changeState(self, stateName):
-        self.currentState = self.states[stateName]
+        self.currentState = stateName
     def turkMachine(self, input):
-        self.tape[0] = input
+        for letter in input:
+            self.tape[0].append(letter)
         l = False
         while self.currentState != self.acceptState:
-            buffer = ''
-            for i in range(len(self.tape)):
-                buffer +=self.headRead(i)
             try:
-                self.states[self.currentState][buffer]
+                buffer = ''
+                for i in range(len(self.tape)):
+                    try:
+                        buffer +=self.headRead(i)
+                    except:
+                        buffer += '_'
+                list = self.states[self.currentState][buffer]
+                for i in list:
+                    i()
             except:
-                print("Rejected")
                 l = True
                 break
         if l is False:
+            print(self.tape)
             print("finished without problems")
+        else:
+            print("eror")
+    def debug(self):
+        print(self.states)
+
+arab = TurMachine("nihilizm.txt")
+arab.turkMachine("BABA_BAB")
+#arab.debug()
